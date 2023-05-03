71c696fbdf29fa07f81d0afc7824b634781c0fb2
Joseph Ordoñez <joseph.ordonez@ucsm.edu.pe>
2023-05-03 11:03:43 -0500

    Prueba de muestra de tabla por interfaz

--- factura/views.py
+++ factura/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.db import connection
+cursor = connection.cursor()
 
 # Create your views here.
+def inicio(req):
+    return render(req, 'inicio.html')
+
+def productos(req):
+    cursor.execute('CALL sp_listar_producto')
+    data = cursor.fetchall()
+    return render(req, 'productos/index.html',{'data': data})
--- hospital/settings.py
+++ hospital/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'factura',
+    'hospital'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +77,12 @@ WSGI_APPLICATION = 'hospital.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'bd_farmacia',
+        'USER':'root',
+        'PASSWORD':'MySQL',
+        'HOST':'localhost',
+        'PORT':'3306'
     }
 }
 
