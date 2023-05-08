b8093f8e45974d3afd85924ce28d2ef4ee3a8071
DIEGOPARDO22 <diego.pardo@ucsm.edu.pe>
2023-05-07 19:06:46 -0500

    modificacion base de datos

--- Script BD/ScriptBD.sql
+++ Script BD/ScriptBD.sql
@@ -4,13 +4,15 @@
 CREATE DATABASE BD_Farmacia;
 USE BD_Farmacia;
 
+
 /*CREACION DE TABLA CLIENTE*/
 CREATE TABLE IF NOT EXISTS cliente(
     ruc CHAR(11) NOT NULL,
     nombresorazon VARCHAR(100) NOT NULL,
     direccion VARCHAR(150),
+    fecha_nacimiento DATE,
     telefono CHAR(9),
-    email VARCHAR(50),
+    email VARCHAR(25),
     PRIMARY KEY (ruc) 
 ) ENGINE=InnoDB;
 
@@ -24,12 +26,13 @@ CREATE TABLE IF NOT EXISTS categoria(
 
 /*CREACTION DE TABLA FACTURA*/
 CREATE TABLE IF NOT EXISTS factura(
-    num_factura CHAR(5) NOT NULL,
+    num_factura SMALLINT NOT NULL AUTO_INCREMENT,
     ruc CHAR(11),
     fecha DATE,
     PRIMARY KEY (num_factura),
-    FOREIGN KEY (ruc) REFERENCES cliente (ruc) ON UPDATE CASCADE ON DELETE SET NULL
+    FOREIGN KEY (ruc) REFERENCES cliente (ruc) ON UPDATE CASCADE
 ) ENGINE=InnoDB;
+ALTER TABLE factura AUTO_INCREMENT=3000;
 
 /*CREACION DE TABLA PRODUCTO*/
 CREATE TABLE IF NOT EXISTS producto(
@@ -45,31 +48,34 @@ ALTER TABLE producto AUTO_INCREMENT=1000;
 
 /*CREACION DE TABLA DETALLE*/
 CREATE TABLE IF NOT EXISTS detalle(
-    num_detalle CHAR(5) NOT NULL,
-    id_factura CHAR(5) NOT NULL,
+    num_detalle SMALLINT NOT NULL AUTO_INCREMENT,
+    id_factura SMALLINT NOT NULL,
     id_producto SMALLINT,
     cantidad INT,
     PRIMARY KEY (num_detalle, id_factura),
-    FOREIGN KEY (id_factura) REFERENCES factura (num_factura),
-    FOREIGN KEY (id_producto) REFERENCES producto (id_producto) ON UPDATE CASCADE ON DELETE SET NULL
+    FOREIGN KEY (id_factura) REFERENCES factura (num_factura) ON UPDATE CASCADE on delete cascade, 
+    FOREIGN KEY (id_producto) REFERENCES producto (id_producto) ON UPDATE CASCADE
 ) ENGINE=InnoDB;
+ALTER TABLE detalle AUTO_INCREMENT=2000;
 
 /*============================================================
                     INSERCIÓN DE DATOS
 ============================================================*/
 
+
+
 /*INSERCION EN TABLA CLIENTE*/
-INSERT INTO cliente (ruc, nombresorazon, direccion, telefono, email) VALUES
-(90453612111, 'Pfizer Inc.', 'Calle Los Pinos #123, Urbanización Las Casuarinas, Arequipa', '998765432', 'srodriguez@gmail.com'),
-(23657849222, 'Novartis AG.', 'Avenida Dolores #456, Urbanización Los Cedros, Yanahuara, Arequipa', '995678521', 'ahernandez@hotmail.com'),
-(75891236333, 'Johnson & Johnson', 'Calle Francisco Bolognesi #678, Urbanización Las Condes, Sachaca, Arequipa', '954221345', 'vcastro@gmail.com'),
-(12345678444, 'Umbrella Corporation', 'Calle Mariscal Castilla #210, Distrito De Challapampa, Arequipa', '994536187', 'dgarcia@hotmail.com'),
-(89765432555, 'AstraZeneca plc.', 'Avenida Ejercito #1234, Urbanización Vila Real, Cayma, Arequipa', '958765432', 'pmartinez@hotmail.com'),
-(65412789666, 'Boehringer Ingelheim', 'Calle Lima #567, Urbanización San Lazaro, Arequipa', '948765212', 'jgonzalez@gmail.com'),
-(43219876777, 'Roche Holding AG', 'Avenida Andres Avelino Caceres #890, Distrito De Hunter, Arequipa', '955678123', 'ndiaz@hotmail.com'),
-(56473829888, 'Eli Lilly and Company', 'Calle Avenida De La Cultura #456, Urbanización Las Vizcachas, Arequipa', '948635184', 'ahernandez@gmail.com'),
-(21897543999, 'Sanofi S.A.', 'Avenida La Marina #789, Distrito De Cerro Colorado, Arequipa', '955786123', 'kperez@hotmail.com'),
-(98765431101, 'Merck & Co., Inc.', 'Calle Juanita #234, Urbanización Los Pinos, Arequipa', '998734123', 'mreyes@gmail.com');
+INSERT INTO cliente (ruc, nombresorazon, direccion,fecha_nacimiento, telefono, email) VALUES
+(90453612111, 'Pfizer Inc.', 'Calle Los Pinos #123, Urbanización Las Casuarinas, Arequipa', '1997-12-03', '998765432', 'srodriguez@gmail.com'),
+(23657849222, 'Novartis AG.', 'Avenida Dolores #456, Urbanización Los Cedros, Yanahuara, Arequipa', '1995-06-21', '995678521', 'ahernandez@hotmail.com'),
+(75891236333, 'Johnson & Johnson', 'Calle Francisco Bolognesi #678, Urbanización Las Condes, Sachaca, Arequipa', '1999-09-05', '954221345', 'vcastro@gmail.com'),
+(12345678444, 'Umbrella Corporation', 'Calle Mariscal Castilla #210, Distrito De Challapampa, Arequipa', '2000-11-17', '994536187', 'dgarcia@hotmail.com'),
+(89765432555, 'AstraZeneca plc.', 'Avenida Ejercito #1234, Urbanización Vila Real, Cayma, Arequipa', '2002-04-02', '958765432', 'pmartinez@hotmail.com'),
+(65412789666, 'Boehringer Ingelheim', 'Calle Lima #567, Urbanización San Lazaro, Arequipa', '1996-08-28', '948765212', 'jgonzalez@gmail.com'),
+(43219876777, 'Roche Holding AG', 'Avenida Andres Avelino Caceres #890, Distrito De Hunter, Arequipa', '1998-02-14', '955678123', 'ndiaz@hotmail.com'),
+(56473829888, 'Eli Lilly and Company', 'Calle Avenida De La Cultura #456, Urbanización Las Vizcachas, Arequipa', '2001-05-30', '948635184', 'ahernandez@gmail.com'),
+(21897543999, 'Sanofi S.A.', 'Avenida La Marina #789, Distrito De Cerro Colorado, Arequipa', '1995-10-11', '955786123', 'kperez@hotmail.com'),
+(98765431101, 'Merck & Co., Inc.', 'Calle Juanita #234, Urbanización Los Pinos, Arequipa', '2003-07-07', '998734123', 'mreyes@gmail.com');
 
 INSERT INTO categoria (id_categoria, nombre, descripcion) VALUES
 ('001', 'Medicamentos', 'Productos para el tratamiento de enfermedades'),
@@ -100,6 +106,8 @@ INSERT INTO producto (nombre, precio, stock, id_categoria) VALUES
 ('Hierro', 8.25, 50, '003'),
 ('Vitamina B12', 7.50, 75, '003');
 
+
+
 INSERT INTO detalle (num_detalle, id_factura, id_producto, cantidad) VALUES
 ('00001', '00001', '1001', 2),
 ('00002', '00001', '1003', 1),
@@ -111,6 +119,12 @@ INSERT INTO detalle (num_detalle, id_factura, id_producto, cantidad) VALUES
 ('00008', '00004', '1004', 1),
 ('00009', '00005', '1001', 1),
 ('00010', '00005', '1003', 2);
+
+SELECT * FROM cliente;
+SELECT * FROM categoria;
+SELECT * FROM factura;
+SELECT * FROM producto;
+SELECT * FROM detalle;
 
 /* =============================================================================================
 											LISTAR
@@ -141,6 +155,13 @@ INSERT INTO producto(nombre, precio, stock, id_categoria) VALUES(in_nombre, in_p
 CREATE PROCEDURE `sp_crear_cliente`(IN in_ruc CHAR(11), IN in_razon VARCHAR(100), IN in_direccion VARCHAR(150), in_telefono CHAR(9), in_email VARCHAR(50))
 INSERT INTO cliente(ruc, nombresorazon, direccion, telefono, email) VALUES(in_ruc, in_razon, in_direccion, in_telefono, in_email);
 
+CREATE PROCEDURE `sp_crear_factura`(IN ruc CHAR(11), IN fecha DATE)
+INSERT INTO factura(ruc, fecha) VALUES(ruc, fecha);
+
+CREATE PROCEDURE `sp_crear_detalle`(in id_factura SMALLINT, in id_producto SMALLINT, in cantidad INT)
+INSERT INTO detalle(id_factura, id_producto, cantidad) VALUES(id_factura, id_producto, cantidad);
+
+
 /* =============================================================================================
 											EDITAR
 ============================================================================================= */ 
@@ -161,6 +182,9 @@ CREATE PROCEDURE `sp_eliminar_producto`(IN in_id_producto SMALLINT)
 DELETE FROM producto
 WHERE id_producto = in_id_producto;
 
+create procedure `sp_eliminar_factura`(IN in_id_factura SMALLINT)
+delete from bd_farmacia.factura where factura.num_factura=in_id_factura;
+
 CREATE PROCEDURE `sp_eliminar_cliente`(IN in_ruc_cliente CHAR(11))
 DELETE FROM cliente
 WHERE ruc = in_ruc_cliente;
@@ -173,4 +197,21 @@ SELECT producto.id_producto, producto.nombre, producto.precio, producto.stock, p
 FROM producto INNER JOIN categoria WHERE id_producto=in_id_producto AND producto.id_categoria=categoria.id_categoria;
 
 CREATE PROCEDURE `sp_buscar_cliente_por_id`(IN in_ruc_cliente CHAR(11))
-SELECT * FROM cliente WHERE in_ruc_cliente = cliente.ruc
+SELECT * FROM cliente WHERE in_ruc_cliente = cliente.ruc;
+
+/* =============================================================================================
+											MOSTRAR
+============================================================================================= */
+create procedure `sp_ver_factura`(IN num_fac smallint)
+SELECT factura.num_factura, factura.fecha, cliente.nombresorazon, cliente.ruc, producto.nombre, detalle.cantidad, producto.precio, cliente.direccion
+FROM detalle
+INNER JOIN factura ON factura.num_factura = detalle.id_factura
+INNER JOIN cliente ON cliente.ruc = factura.ruc
+INNER JOIN producto ON producto.id_producto = detalle.id_producto where
+factura.num_factura=num_fac;
+
+call sp_ver_factura(1);
+/* =============================================================================================
+											ULTIMA FACTURA
+============================================================================================= */
+create procedure `sp_ver_ultima_factura`() SELECT MAX(num_factura) FROM factura;
